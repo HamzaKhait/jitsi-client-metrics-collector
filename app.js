d4daf020e701e1ac0ad5e4d220fb93f58138f0e0
Hamza KHAIT <hamzakhait@gmail.com>
2024-07-18 16:41:05 +0200

    fix: adding dynamic push url one the client side

--- app.js
+++ app.js
@@ -19,6 +19,10 @@ var app = express();
 //   next();
 // });
 
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
 app.set('trust proxy', ['loopback', 'linklocal', 'uniquelocal']) // trust reverse proxies from the following subnets
 
 app.use(helmet()); //TODO add more security rules
@@ -26,11 +30,29 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 var indexRouter = require('./routes/index');
 app.use('/', indexRouter);
 var metricsRouter = require('./routes/metrics');  
 app.use('/metrics', metricsRouter);
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -8,4 +8,10 @@ router.get('/', function(req, res, next) {
   res.send(200);
 });
 
+router.get('/client-file', function(req, res, next) {
+  res.setHeader('Content-Type','text/javascript; charset=UTF-8');
+  res.render('jcmc-client-file', { jcmc_push_url: appConfig.ALLOW_PRIVATE_SCRAPE_ONLY });
+});
+
+
 module.exports = router;
