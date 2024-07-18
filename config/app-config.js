25bcfd38609a5401f665206dc7a2b0bd6be3cc9d
Hamza KHAIT <hamzakhait@gmail.com>
2024-07-18 17:18:21 +0200

    fix: correcting PUSH_URL

--- config/app-config.js
+++ config/app-config.js
@@ -6,7 +6,7 @@ const appConfig = {
   METRICS_EXPIRATION_CHECK: parseInt(process.env.METRICS_EXPIRATION_TOLERANCE) || 10,
   METRICS_EXPIRATION: parseInt(process.env.METRICS_EXPIRATION) || 30,
   ALLOW_PRIVATE_SCRAPE_ONLY: toBool(process.env.ALLOW_PRIVATE_SCRAPE_ONLY) || false,
-  ALLOW_PRIVATE_SCRAPE_ONLY: toBool(process.env.PUSH_URL) || "http://127.0.0.1:3000/metrics/push"
+  PUSH_URL: process.env.PUSH_URL || "http://127.0.0.1:3000/metrics/push"
 };
 
 module.exports = appConfig;
